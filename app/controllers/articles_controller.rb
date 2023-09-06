d075f648e308e91f00b70d35deb1d683ebe090c7
ali <ali-shah22@hotmail.com>
2023-09-07 01:19:18 +0500

    Editing, Updating and Deleting Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,11 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+
+    end
+
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         if @article.save
@@ -21,5 +26,23 @@ class ArticlesController < ApplicationController
           end
     end
 
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+          flash[:notice] = "Article was updated successfully."
+          redirect_to @article
+        else
+          render 'edit'
+        end
+
+    end
+
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+
+    end
 
 end
